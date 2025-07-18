9d6a89a27e75ee893cb163b7bc7ac6709679267b
Piotr1480 <56109097+Piotr1480@users.noreply.github.com>
2025-07-18 11:17:53 +0200

    Modyfikacja JS

--- js/commonJS.js
+++ js/commonJS.js
@@ -8,28 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
         navMenuContainer.classList.toggle('active');
     });
 
-    //
-    document.addEventListener('DOMContentLoaded', function() {
-        const hamburger = document.getElementById('hamburger');
-        const navMenuContainer = document.querySelector('.nav-menu-container');
-
-        // Obsługa kliknięcia w linki menu głównego (z wyjątkiem "Technologie")
-        const navLinks = document.querySelectorAll('.topNav a');
-        navLinks.forEach(link => {
-            link.addEventListener('click', function(event) {
-                // Jeśli to submenu — otwórz stronę
-                if (link.closest('.submenu')) {
-                    event.preventDefault(); // zapobiega domyślnemu przeładowaniu
-                    const targetUrl = link.getAttribute('href');
-                    window.location.href = targetUrl; // przejście do strony
-                }
-
-                // Zamykanie menu (nie zamykaj przy kliknięciu w "Technologie")
-                if (!link.closest('.has-submenu')) {
-                    hamburger.classList.remove('active');
-                    navMenuContainer.classList.remove('active');
-                }
-            });
+    // Zamknij menu po kliknięciu na link
+    const navLinks = document.querySelectorAll('.topNav a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            hamburger.classList.remove('active');
+            navMenuContainer.classList.remove('active');
         });
     });
 
